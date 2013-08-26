d1e7811f6f856c84762b7f0757c3a4a31af0ef18
John Bresnahan <jbresnah@redhat.com>
2013-08-26 13:41:41 -1000

    Make it run and add an object for clarity
    Prior to this change some API calls were broken.

--- gitcovdiff/__init__.py
+++ gitcovdiff/__init__.py
@@ -1,35 +1,42 @@
-from __future__ import print_function
 import os
 import sys
 
-import pygit2
 import coverage
+import pygit2
 
 
-def collect_addition(hunk):
-    result = []
-    context_or_addition = filter(lambda l: l[1] in (pygit2.GIT_DIFF_LINE_CONTEXT,
-                                                    pygit2.GIT_DIFF_LINE_ADDITION),
-                                 hunk.data)
-    for index, line in enumerate(context_or_addition):
-        if line[1] == pygit2.GIT_DIFF_LINE_ADDITION:
-            result.append(hunk.new_start + index)
-    return result
+class AlteredFile(object):
+    def __init__(self, hunk, cov, basedir):
+        self.file_name = hunk.new_file_path
+        self.hunk = hunk
+        self.new_lines = self._get_new_lines()
+        self.total_missing_lines = self._get_uncovered_lines(cov, basedir)
+        self.missing = list(set(self.new_lines) & set(self.total_missing_lines))
+        self.missing.sort()
+
+    def _get_new_lines(self):
+        new_lines = []
+        for h in self.hunk.hunks:
+            line_index = 0
+            for l in h.lines:
+                if l[0] == u'+':
+                    new_lines.append(h.new_start + line_index)
+                line_index += 1
+        return new_lines
+
+    def _get_uncovered_lines(self, cov, basedir):
+        hunk_path = os.path.join(basedir, self.hunk.new_file_path)
+        missing_list = cov.analysis2(hunk_path)
+        return missing_list[3]
 
 
 def find_missing_appear(cov, repo):
-    head = repo.head
-    for parent in head.parents:
-        diff = parent.tree.diff(head.tree)
-        for hunk in diff.changes['hunks']:
-            try:
-                missing = cov.analysis(os.path.join(repo.workdir, hunk.new_file))[2]
-            except coverage.misc.NotPython:
-                continue
-            addition = collect_addition(hunk)
-            missing_appear = set(missing) & set(addition)
-            if missing_appear:
-                yield hunk, missing_appear
+    commit = repo.head.get_object()
+    for parent in commit.parents:
+        diff = parent.tree.diff_to_tree(commit.tree)
+        for hunk in diff:
+            altered_file = AlteredFile(hunk, cov, repo.workdir)
+            yield altered_file
 
 
 FAIL = '\033[91m'
@@ -37,36 +44,9 @@ ENDC = '\033[0m'
 
 
 def report(cov, repo):
-    head = repo.head
-    for hunk, missing_appear in find_missing_appear(cov, repo):
-        missing_index = map(lambda num: num - hunk.new_start + 1,
-                            missing_appear)
-        print(hunk.new_file)
-        print(hunk.header)
-        i = 0
-        for line in hunk.data:
-            line_stat = line[1]
-            if line_stat in (pygit2.GIT_DIFF_LINE_CONTEXT,
-                             pygit2.GIT_DIFF_LINE_ADDITION):
-                i += 1
-            if line_stat == pygit2.GIT_DIFF_LINE_CONTEXT:
-                prefix = " "
-            elif line_stat == pygit2.GIT_DIFF_LINE_ADDITION:
-                prefix = "+"
-            elif line_stat == pygit2.GIT_DIFF_LINE_DELETION:
-                prefix = "-"
-            else:
-                # XXX maybe some unknown stats..
-                print("Unknown stat: {}".format(line_stat), file=sys.stderr)
-                continue
-
-            line_data = line[0].rstrip()
-
-            if i in missing_index and line_stat != pygit2.GIT_DIFF_LINE_DELETION:
-                # found the line missing appear
-                print(FAIL + "{0}{1}".format(prefix, line_data) + ENDC)
-            else:
-                print("{0}{1}".format(prefix, line_data))
+    for altered_file in find_missing_appear(cov, repo):
+        print altered_file.file_name
+        print "\t %s" % str(list(altered_file.missing))
 
 
 def main():
@@ -78,14 +58,14 @@ def main():
     _git = os.path.join(target_repository, '.git')
     _coverage = os.path.join(target_repository, '.coverage')
     if not os.path.isdir(_git):
-        raise IOError(".git not exists")
+        raise IOError(".git does not exist in the directory %s" % _git)
     if not os.path.exists(_coverage):
-        raise IOError(".coverage not exists")
+        raise IOError(
+            ".coverage does not exists in the directory %s" % _coverage)
 
     repo = pygit2.Repository(_git)
     cov = coverage.coverage(_coverage)
     cov.load()
-
     report(cov, repo)
 
 
